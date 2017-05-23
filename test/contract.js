67a07bdb9217322fdc05785a278264e1d5fb5841
Richard Schneider <makaretu@gmail.com>
2017-05-23 18:36:01 +1200

    fix(contract): overtricks

--- test/contract.js
+++ test/contract.js
@@ -17,6 +17,13 @@ describe('Contract', function() {
 
         contract.risk = 'X';
         expect(scorer.contract(contract, false, 2)).to.equal(470);
+        
+        contract = {
+            level: 2,
+            denomination: 'C',
+            risk: ''
+        };
+        expect(scorer.contract(contract, true, 2)).to.equal(90);        
     });
 
     it('scores passed in', function() {
@@ -32,10 +39,14 @@ describe('Contract', function() {
             risk: ''
         };
         expect(scorer.contract(contract, false, -3)).to.equal(-150);
+        expect(scorer.contract(contract, true, -3)).to.equal(-300);
 
         contract.risk = 'X';
         expect(scorer.contract(contract, false, -3)).to.equal(-500);
         expect(scorer.contract(contract, true, -3)).to.equal(-800);
+
+        contract.risk = 'XX';
+        expect(scorer.contract(contract, false, -3)).to.equal(-1000);
     });
     
     it('scores overtricks', function() {
@@ -49,8 +60,17 @@ describe('Contract', function() {
         contract = {
             level: 5,
             denomination: 'S',
-            risk: 'XX'
+            risk: ''
         };
+        expect(scorer.contract(contract, false, 6)).to.equal(480);
+        expect(scorer.contract(contract, true, 6)).to.equal(680);
+        
+        contract.risk = 'X';
+        expect(scorer.contract(contract, false, 6)).to.equal(750);
+        expect(scorer.contract(contract, true, 6)).to.equal(1050);
+
+        contract.risk = 'XX';
+        expect(scorer.contract(contract, false, 6)).to.equal(1200);
         expect(scorer.contract(contract, true, 6)).to.equal(1600);
         
         contract = {
@@ -59,6 +79,34 @@ describe('Contract', function() {
             risk: ''
         };
         expect(scorer.contract(contract, false, 7)).to.equal(1020);
+        
+         contract = {
+            level: 5,
+            denomination: 'C',
+            risk: ''
+        };
+        expect(scorer.contract(contract, false, 6)).to.equal(420);
+        expect(scorer.contract(contract, true, 6)).to.equal(620);
+    });
+
+    it('scores grand slams', function() {
+        var contract = {
+            level: 7,
+            denomination: 'S',
+            risk: ''
+        };
+        expect(scorer.contract(contract, false, 7)).to.equal(1510);
+        expect(scorer.contract(contract, true, 7)).to.equal(2210);
+    });
+
+    it('scores small slams', function() {
+        var contract = {
+            level: 6,
+            denomination: 'D',
+            risk: ''
+        };
+        expect(scorer.contract(contract, false, 6)).to.equal(920);
+        expect(scorer.contract(contract, true, 6)).to.equal(1370);
     });
 
 });
