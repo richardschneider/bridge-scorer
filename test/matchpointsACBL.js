5405d198586fcd717ca615bfd1a4ea2e4948103c
Richard Schneider <makaretu@gmail.com>
2017-05-25 15:18:48 +1200

    test(matchpointsACBL): scores fun bridge

--- test/matchpointsACBL.js
+++ test/matchpointsACBL.js
@@ -37,5 +37,30 @@ describe('Matchpoints ACBL', function() {
         expect(games[5]).to.have.property('matchpointsEW').and.eql({value: 6, rank: '1'});
         expect(games[6]).to.have.property('matchpointsEW').and.eql({value: 5, rank: '2'});
     });
+
+    it('scores Fun Bridge', function() {
+        // see https://www.funbridge.com/en/help_faq/touch/scores
+        var games = [
+            { contract: { declaror: 'N' }, score: 650 },
+            { contract: { declaror: 'N' }, score: 620 },
+            { contract: { declaror: 'N' }, score: 620 },
+            { contract: { declaror: 'N' }, score: 620 },
+            { contract: { declaror: 'N' }, score: 620 },
+            { contract: { declaror: 'N' }, score: 170 },
+            { contract: { declaror: 'N' }, score: 170 },
+            { contract: { declaror: 'N' }, score: 140 },
+            { contract: { declaror: 'N' }, score: 110 }
+        ];
+        scorer.matchpointsACBL(games);
+        expect(games[0].matchpointsNS.value).to.equal(8);
+        expect(games[1].matchpointsNS.value).to.equal(5.5);
+        expect(games[2].matchpointsNS.value).to.equal(5.5);
+        expect(games[3].matchpointsNS.value).to.equal(5.5);
+        expect(games[4].matchpointsNS.value).to.equal(5.5);
+        expect(games[5].matchpointsNS.value).to.equal(2.5);
+        expect(games[6].matchpointsNS.value).to.equal(2.5);
+        expect(games[7].matchpointsNS.value).to.equal(1);
+        expect(games[8].matchpointsNS.value).to.equal(0);
+    });
  
 });
